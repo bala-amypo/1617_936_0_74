1ad33cd2fd8deb0ec9e844db204302ac507e7b8a
bala-amypo <bala@amypo.in>
2025-12-18 08:31:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/validationService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.validationEntity;
+import java.util.List;
+
+
+public interface validationService{
+    validationEntity postData(validationEntity val);
+    List<validationEntity> getAllData();
+    String deleteValue(Long id);
+    validationEntity getById(Long id);
+    validationEntity updateData(Integer id,studentEntity stu);
+    
+}
