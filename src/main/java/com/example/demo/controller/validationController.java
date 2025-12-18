e59daa3bea26c10435bbbe347df5249ff6f54e5e
bala-amypo <bala@amypo.in>
2025-12-18 08:41:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/validationController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import com.example.demo.service.validationService;
+import com.example.demo.entity.validationEntity;
+import java.util.List;
+
+
+@RestController
+public class validationController{
+    @Autowired validationService ser;
+    @PostMapping("/valpost")
+    public validationEntity sendData(@RequestBody validationEntity val){
+        return ser.postData(val);
+    }
+    @GetMapping("/valget")
+    public List<validationEntity> getData(){
+        return ser.getAllData();
+    }
+    @DeleteMapping("/valdelete/{id}")
+    public String deleteData(@PathVariable Long id){
+         return ser.deleteValue(id);
+    }
+    @GetMapping("/idget/{id}")
+    public validationEntity getOne(@PathVariable Long id){
+        return ser.getById(id);
+    }
+    @PutMapping("/stuput/{id}")
+    public validationEntity putData(@PathVariable Long id,@RequestBody ValidationEntity val){
+        return ser.updateData(id,val);
+    }
+}
