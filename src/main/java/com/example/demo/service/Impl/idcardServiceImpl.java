ed9e06c2942149e9ab3ac72bcdb6010b5cae6b81
bala-amypo <bala@amypo.in>
2025-12-19 10:41:09 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/idcardServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/idcardServiceImpl.java
@@ -2,7 +2,7 @@ package com.example.demo.service.Impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.demo.entity.idcard;
+import com.example.demo.entity.idcardEntity;
 import com.example.demo.service.idcardService;
 import com.example.demo.repository.idcardRepository;
 import java.util.List;
