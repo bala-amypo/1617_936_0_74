cf89492c5c00965d37ddab621e85371ba300d431
bala-amypo <bala@amypo.in>
2025-12-19 06:30:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/validationEntity.java
+++ src/main/java/com/example/demo/entity/validationEntity.java
@@ -9,6 +9,10 @@ import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Positive;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import 
 
 
 @Entity
@@ -27,49 +31,10 @@ public class validationEntity{
     @Positive(message="Age must be a positive number")
     @Max(30)
     private int age;
-    public void setId(Long id){
-        this.id=id;
-    }
-    public void setUsername(String username){
-        this.username=username;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-    public void setAge(int age){
-        this.age=age;
-    }
-    public Long getId(){
-        return id;
-    }
-    public String getUsername(){
-        return username;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public int getAge(){
-        return age;
-    }
-    public validationEntity(){
-
-    }
-    public validationEntity(Long id,
-        @NotNull @Size(min=2,max=10,message="must be 2 to 10 characters") String username,
-        @Email(message="Email is not valid")  String email,
-        @Size(min=3,max=6,message="must be 3 to 6 characters") @NotNull(message="Password is mandatory")String password,
-        @Positive(message="Age must be a positive number") @Max(30)int age){
-        this.id=id;
-        this.username=username;
-        this.email=email;
-        this.password=password;
-        this.age=age;
-    }
+    @Data
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
 
 }
