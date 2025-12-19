3b829d1d9bacad06078a5b597761ec872cfc57f9
bala-amypo <bala@amypo.in>
2025-12-19 09:17:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/studentEntity.java
+++ src/main/java/com/example/demo/entity/studentEntity.java
@@ -14,11 +14,7 @@ import lombok.NoArgsConstructor;
 
 
 @Entity
-@Data
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class studentEntity{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -27,5 +23,56 @@ public class studentEntity{
     private String email;
     private String password;
     private Date createAt;
-    
+    public User() {
+    }
+
+    public User(Integer id, String username, String email, String password, Date createAt) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.createAt = createAt;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
 }
