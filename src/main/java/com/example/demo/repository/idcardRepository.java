acd4d1768ab39861e4417416a6a8a1e5e415a3ca
bala-amypo <bala@amypo.in>
2025-12-19 10:43:44 +0000

    User commit

--- src/main/java/com/example/demo/repository/idcardRepository.java
+++ src/main/java/com/example/demo/repository/idcardRepository.java
@@ -2,7 +2,7 @@ package com.example.demo.repository;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.idcardtEntity;
+import com.example.demo.entity.idcardEntity;
 
 @Repository
 public interface idcardRepository extends JpaRepository<idcardEntity,Integer>{
