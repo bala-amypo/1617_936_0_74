03bc6722ccfb8a2f1e73fcfd315b74880409ddfc
bala-amypo <bala@amypo.in>
2025-12-19 07:44:00 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/timestampService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.timestamp;
+import java.util.List;
+
+
+public interface timestampService{
+    timestamp postData(timestamp stamp);
+    // List<validationEntity> getAllData();
+    // String deleteValue(Long id);
+    timestamp getById(Long id);
+    // validationEntity updateData(Long id,validationEntity val);
+    
+}
