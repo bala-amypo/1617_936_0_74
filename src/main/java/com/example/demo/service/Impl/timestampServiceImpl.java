20b19cc9551ddefbe3ac713359feef20eb67a11c
bala-amypo <bala@amypo.in>
2025-12-19 07:45:59 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/timestampServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.demo.service.Impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.validationEntity;
+import com.example.demo.service.validationService;
+import com.example.demo.repository.validationRepository;
+import com.example.demo.exception.validationException;
+import java.util.List;
+
+
+@Service 
+public class timestampServiceImpl implements validationService{
+    @Autowired timestampRepository valid;
+    //save(),findAll(),findById(),deleteById(),existsById()
+    @Override
+    public validationEntity postData(validationEntity val){
+        return valid.save(val);
+    }
+    // @Override
+    // public List<validationEntity> getAllData(){
+    //     return valid.findAll();
+    // }
+    // @Override
+    // public String deleteValue(Long id){
+    //     valid.deleteById(id);
+    //     return "Deleted Successfully";
+    // }
+    // @Override
+    // public validationEntity getById(Long id){
+    //     return valid.findById(id).orElseThrow(()->new validationException("invalid Id"+id));
+    // }
+    // @Override
+    // public validationEntity updateData(Long id,validationEntity val){
+    //     if(valid.existsById(id)){
+    //         val.setId(id);
+    //         return valid.save(val);
+    //     }
+    //     return null;
+    // }
+}                                                                               
